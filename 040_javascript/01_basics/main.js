32ff4b91898475a8055f472979bf751bd07d9089
N-showtime <hozukl.1225@gmail.com>
2026-01-25 13:36:06 +0900

    js講義１

---
+++ 040_javascript/01_basics/main.js
@@ -0,0 +1,12 @@
+const num1 = 10;
+const num2 = 5;
+
+const sum = num1 + num2;
+const diff = num1 - num2;
+const product = num1 * num2;
+const quotient = num1 / num2;
+
+console.log(`足し算： ${sum}`);
+console.log(`引き算： ${diff}`);
+console.log(`掛け算： ${product}`);
+console.log(`割り算： ${quotient}`);
