d1e5b4b6aedb4e549dcf88c699dbd29a25575084
N-showtime <hozukl.1225@gmail.com>
2026-01-27 22:26:41 +0900

    js機能を実装

--- week1_homework/assets/js/app.js
+++ week1_homework/assets/js/app.js
@@ -3,4 +3,23 @@ particlesJS.load('particles-js', 'assets/js/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 
-const cards = document.querySelectorAll('.skill-card');
+const cards = document.querySelector('.skill-card');
+
+// カウンターの設定
+const counters = document.querySelector('.btn-count');
+const likeButton = document.querySelector('.button');
+let count =0;
+
+likeButton.addEventListener("click", function () {
+  count++;
+  counters.textContent = count;
+});
+
+// スキルカードの表示・非表示
+const skillCards = document.querySelectorAll('.skill-card');
+
+skillCards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('is-open');
+  });
+});
